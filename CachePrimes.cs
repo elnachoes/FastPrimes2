75470d2f0cf2cf8f0f9373d277ffe746ac9372be
elnachoes <corbin.martin@protonmail.com>
2021-12-08 15:46:15 -0800

    added in a cacheing primes but it doesn't quite work.

---
+++ CachePrimes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FastPrimes2
+{
+    public static class CachePrimes
+    {
+        public static void Cache(List<UInt32> FoundPrimes, UInt32 TargetPrime)
+        {
+            FoundPrimes = FoundPrimes.OrderBy(p => p).Take(((int)TargetPrime)).ToList();
+            TextWriter textWriter = new StreamWriter("CachedPrimes.txt");
+            foreach (var Prime in FoundPrimes)
+            {
+                textWriter.WriteLine(Prime);
+            }
+            textWriter.Close();
+        }
+
+        public static List<UInt32> Load()
+        {
+            List<UInt32> CachePrimes = new List<UInt32>();
+            TextReader textReader = new StreamReader("CachedPrimes.txt");
+            while (textReader.ReadLine() != null)
+            {
+                CachePrimes.Add(Convert.ToUInt32(textReader.ReadLine()));
+            }
+            return CachePrimes;
+        }
+    }
+}
